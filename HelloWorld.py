37f2c86f5112eeeba7dc481c79df176d57ebfff4
matthewnelsonn <112517056+matthewnelsonn@users.noreply.github.com>
2022-09-02 10:56:35 -0600

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,7 @@
+print("Hello, world!")
+
+qustion = input("What's your name? ")
+print(qustion)
+print("That is a beautiful name. Nice to meet you")
+name = ("Gertrude; it's a family name!")
+print("My name is", name)
